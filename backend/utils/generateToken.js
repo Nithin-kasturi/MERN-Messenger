40ff0dd91516634cf6fa0307b60f25ed477685ef
Nithin-kasturi <nithinkasturi8@gmail.com>
2024-04-07 13:16:52 +0530

    Deployment Changes

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -7,6 +7,7 @@ const generateToken=(userId,res)=>{
         maxAge:15*24*60*60*1000,
         httpOnly:true,
         sameSite:"strict",
+        secure:process.env.NODE_ENV!=="development",
     })
 };
 export default generateToken;
